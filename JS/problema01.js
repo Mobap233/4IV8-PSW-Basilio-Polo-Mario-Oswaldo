5635738c9033f03ef2e6b7980cd7913b0d05b854
Mobap233 <mbasiliop1900@alumno.ipn.mx>
2021-03-25 19:40:14 -0600

    Validacion del Problema 01

--- JS/problema01.js
+++ JS/problema01.js
@@ -24,12 +24,35 @@ function validarn(e){
 
 }
 
+function validarm(e){
+
+    var teclado = (document.all)?e.keyCode:e.which
+
+    // == 8 código de la tecla númerica
+    if(teclado == 8)return true;
+
+
+    //diagonales invirtidas dicen algo.
+    var patron = /[0-9]/
+
+    var prueba = String.fromCharCode(teclado);
+    return patron.test(prueba);
+
+}
+
 function interes(){
 
     var valor = document.formulario.cantidad.value;
+    var mes = document.formulario.meses.value;
+
+    var mes_1 = parseInt(mes)
     var result= parseInt(valor);
-    var interes = result * 0.02;
-    var total = result+interes;
+
+
+    var interes = result*(mes_1*0.02);
+
+
+    var total = result+mes_1+interes;
 
     document.formulario.sueldoti.value = "$" +total;
 
@@ -41,5 +64,7 @@ function borrar(){
 
     document.formulario.sueldoti.value = "";
     document.formulario.cantidad.value = "";
+    document.formulario.meses.value = "";
+
 
 }
